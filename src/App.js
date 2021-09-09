5b81ff7c927df950dcaf70a98190cf3d0441e92c
임빈영 <qlsdud960604@gmail.com>
2021-09-09 13:40:16 +0900

    useState, 렌더링 실행 조건 학습

--- src/App.js
+++ src/App.js
@@ -1,31 +1,77 @@
+import { useState } from 'react';
 import './App.css';
 
-let a = 10; // 변수
-const b = 20; // 상수
-
 function App() {
-  let c = [1, 2, 3];
+  /* 일반적인 변수의 사용 */
+  // let number = 1;
+
+  // const add = () => {
+  //   number++;
+  //   console.log(number);
+  // };
+
+  /* useState의 사용 */
+  // const [number, setNumber] = useState(1);
+
+  // const add = () => {
+  //   setNumber(number + 1);
+  //   console.log(number);
+  // };
+
+  /* 상태값이 동일해도 레퍼런스가 다르다면 다시 렌더링 됨 */
+  // console.log('App 실행됨');
+
+  // const [users, setUsers] = useState([
+  //   { id: 1, name: '홍길동' },
+  //   { id: 2, name: '임꺽정' },
+  //   { id: 3, name: '이순신' },
+  //   { id: 4, name: '장보고' },
+  // ]);
+
+  // const download = () => {
+  //   let sample = [
+  //     { id: 1, name: '홍길동' },
+  //     { id: 2, name: '임꺽정' },
+  //     { id: 3, name: '이순신' },
+  //     { id: 4, name: '장보고' },
+  //   ];
+
+  //   setUsers(sample);
+  // };
+
+  /* 상태값이 변경되어도 레퍼런스가 동일하다면 다시 렌더링 되지 않음 */
+  let sample = [
+    { id: 1, name: '홍길동' },
+    { id: 2, name: '임꺽정' },
+    { id: 3, name: '이순신' },
+    { id: 4, name: '장보고' },
+  ];
+
+  const [users, setUsers] = useState(sample);
+
+  const download = () => {
+    sample.push({ id: 5, name: '강감찬' });
+
+    setUsers(sample);
+  };
+
   return (
+    // <div>
+    //   <h1> 숫자 : {number}</h1>
+    //   <button onClick={add}>더하기</button>
+    // </div>
     <div>
-      <div style={myStyle}>
-        안녕 {a === 10 ? '10입니다.' : '10이 아닙니다.'}
-      </div>
-      <h1 className="box-style">해딩태그 {b === 20 && '20입니다.'}</h1>
-      <hr />
-      <div>
-        {c.map((n) => (
-          <h1>{n}</h1>
-        ))}
-      </div>
+      <button onClick={download}>다운로드</button>
+      {users.map((user) => (
+        <h1>
+          {user.id}, {user.name}
+        </h1>
+      ))}
     </div>
   );
 }
 
 export default App;
-
-const myStyle = {
-  color: 'red',
-};
 
 // 1. React는 리턴 시 하나의 Dom(태그)만 리턴할 수 있음
 // 2. 변수선언은 let 혹은 const로만 해야함 (var의 경우 변수의 scope가 일반적인 언어와는 다름)
