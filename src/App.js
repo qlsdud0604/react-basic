1e785de0846b371b27fb34841f3a486b5dba5702
임빈영 <qlsdud960604@gmail.com>
2021-09-09 18:29:18 +0900

    useRef 학습

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useRef } from 'react';
 import './App.css';
 
 function App() {
-  const [list, setList] = useState([1, 2, 3, 4]);
-  const [string, setString] = useState('합계');
-
-  const getAddResult = () => {
-    let sum = 0;
-
-    list.forEach((i) => (sum += i));
-    console.log(sum);
-
-    return sum;
-  };
-
-  const addResult = useMemo(() => getAddResult(), [list]);
+  const myRef = useRef(null);
 
   return (
     <div>
       <button
         onClick={() => {
-          setString('안녕');
+          myRef.current.style.backgroundColor = 'red';
         }}
       >
-        문자 변경
+        색 변경
       </button>
-
-      <button
-        onClick={() => {
-          setList([...list, 10]);
-        }}
-      >
-        리스트값 추가
-      </button>
-      <div>
-        {list.map((item) => (
-          <h1>{item}</h1>
-        ))}
-      </div>
-      <div>
-        {string} : {addResult}
-      </div>
+      <div ref={myRef}>박스</div>
     </div>
   );
 }
